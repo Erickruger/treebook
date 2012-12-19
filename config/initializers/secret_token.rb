1cc855997b2b311b95cecaba029e9bcd90a987d1
Erik Kruger <erickruger84@gmail.com>
2012-12-19 19:03:25 +0200

    Created a rails application and added status ipdates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '32ee4f65ad09bc26d92173bf38792e715e7adf48c910e525884ccddad0dda642b0c9c5c497b719179c323f2a005131f50bbabf597e8377e03f32162db2956917'
